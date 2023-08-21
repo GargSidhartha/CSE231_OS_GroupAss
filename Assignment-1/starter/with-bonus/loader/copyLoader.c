5a76ba8e9c7e663d2e932f0e2b4cd14d67debf39
Saksham Singh <saksham5904@gmail.com>
2023-08-21 10:57:37 +0530

    allocated memory, thinking of copyng segment

--- Assignment-1/starter/with-bonus/loader/copyLoader.c
+++ Assignment-1/starter/with-bonus/loader/copyLoader.c
@@ -2,14 +2,18 @@
 
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
+Elf32_Phdr * segmntHdr; 
 int fd;
 
 /*
  * release memory and other cleanups
  */
-// void loader_cleanup() {
-  
-// }
+void loader_cleanup() {
+  free(ehdr);
+  free(phdr);
+  free(segmntHdr);
+  close(fd);
+}
 
 /*
  * Load and run the ELF executable file
@@ -36,31 +40,34 @@ int main() {
     phdr = malloc(ehdr -> e_phnum * sizeof(Elf32_Phdr));
     read(fd,phdr,sizeof(Elf32_Phdr));
     if (phdr -> p_type == 1 && phdr -> p_vaddr <= ehdr -> e_entry && phdr -> p_vaddr + 0x1000 > ehdr -> e_entry){
-      break;
+      segmntHdr = phdr;
     }
   }
   // second program header check 
-  printf("%x\n", phdr->p_offset);
+  printf("%x\n", phdr->p_align);
 
   // 3. Allocate memory of the size "p_memsz" using mmap function 
   //    and then copy the segment content
+  void * virtual_mem = mmap(NULL, phdr->p_memsz, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANONYMOUS|MAP_PRIVATE, 0, 0);
 
-  Elf32_Phdr * segment = mmap(NULL, phdr->p_memsz, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANONYMOUS|MAP_PRIVATE, 0, 0);
-  segment -> p_align = phdr -> p_align;
-  segment -> p_vaddr = phdr -> p_vaddr;
-  segment -> p_memsz = phdr -> p_memsz;
-  segment -> p_paddr = phdr -> p_paddr;
-  segment -> p_type = phdr -> p_type;
-  segment -> p_offset = phdr -> p_offset;
-  segment -> p_filesz = phdr -> p_filesz;
+  // segment -> p_align = phdr -> p_align;
+  // segment -> p_vaddr = phdr -> p_vaddr;
+  // segment -> p_memsz = phdr -> p_memsz;
+  // segment -> p_paddr = phdr -> p_paddr;
+  // segment -> p_type = phdr -> p_type;
+  // segment -> p_offset = phdr -> p_offset;
+  // segment -> p_filesz = phdr -> p_filesz;
 
 
   // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
-  printf("%x\n", segment->p_memsz);
+  // printf("%x\n", segment->p_memsz);
 
   // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
   // 6. Call the "_start" method and print the value returned from the "_start"
-//   int result = _start();
-//   printf("User _start return value = %d\n",result);
+
+
+  // int result = _start();
+  // printf("User _start return value = %d\n",result);
+
   return 0;
 }
