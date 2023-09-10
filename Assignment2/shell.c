8631801c73f3400379fb10b6e2149295c266cb68
Saksham Singh <saksham5904@gmail.com>
2023-09-10 22:56:40 +0530

    readme init

--- Assignment2/shell.c
+++ Assignment2/shell.c
@@ -1,4 +1,26 @@
-#include "headers.h"
+#include<stdio.h>
+#include<string.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<time.h>
+#include<stdbool.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+#define COMLEN 1000
+#define COMHISLEN 100 
+
+#define clear() printf("\033[H\033[J")
+
+int launch(char** args, int arg_num, bool is_pipe, int history_size, char** command_history);
+
+int execute(char* command,char** command_history,int history_size, bool is_pipe);
+
+int execute_pip(char* command, char** command_history, int history_size);
+
+int shInterpreter(char* shFile, char** command_history, int history_size);
+
 
 typedef struct command_info{
     int pid;
@@ -16,7 +38,7 @@ void init_shell()
 {
     clear();
     printf("\n\n\n\n---------------------------------------------------");
-    printf("\n\n\n\t****A SimpleSHELL****");
+    printf("\n\n\n\t**** ShellVetica ****");
     printf("\n\n\t-SakshsamSingh\n\tSidharthaGarg-");
     printf("\n\n\n\n---------------------------------------------------");
     char* username = getenv("USER");
@@ -239,7 +261,7 @@ int main(){
     do{
         char* user = getenv("USER");
         
-        printf("\n%s:~$ > ",user);
+        printf("\n%s@shellvetica:~$ > ",user);
     
         char command[COMLEN];
 
