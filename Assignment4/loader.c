2d405cb9b0ea869f9772a3ff23c7e62656f3572b
GargSidhartha <gsidhartha53@gmail.com>
2023-11-01 23:45:11 +0530

    something

--- Assignment4/loader.c
+++ Assignment4/loader.c
@@ -1,13 +1,10 @@
 #include "loader.h"
+#include <signal.h>
 
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
 Elf32_Phdr * segmntHdr; 
 int fd;
-int pageFault = 0;
-int pageAllocated = 0;
-void * virtual_mem;
-
 
 /*
  * release memory and other cleanups
@@ -65,7 +62,40 @@ int checkELFIdent(Elf32_Ehdr * ehdr) {
     return 1;
   }
 }
+Elf32_Phdr *find_segment_by_address(Elf32_Addr address) {
+    for (int i = 0; i < ehdr->e_phnum; i++) {
+        Elf32_Phdr *segment = &phdr[i];
+        if (address >= segment->p_vaddr && address < (segment->p_vaddr + segment->p_memsz)) {
+            return segment;
+        }
+    }
+    return NULL; // Address doesn't belong to any segment
+}
+void seg_handler(int signum, siginfo_t *info, void *context) {
+    printf("Segmentation fault at address %p\n", info->si_addr);
 
+    // Find the segment associated with the faulting address
+    segmntHdr = find_segment_by_address((Elf32_Addr)info->si_addr);
+
+    // if (segmntHdr) {
+    //     // Allocate memory using mmap at the faulting address
+    //     void *virtual_mem = mmap(info->si_addr, segmntHdr->p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED, 0, 0);
+    //     if (virtual_mem == MAP_FAILED) {
+    //         perror("Error mapping memory");
+    //         exit(1);
+    //     }
+
+    //     // Copy the content of the segment into the newly allocated memory
+    //     copy_segment_content(virtual_mem, segmntHdr);
+
+    //     // Resume execution
+    //     return;
+    // } else {
+    //     // Unable to find the segment, terminate the program
+    //     printf("Unable to find the associated segment. Terminating.\n");
+    //     exit(1);
+    // }
+}
 /*
  * Load and run the ELF executable file
  */
@@ -94,23 +124,32 @@ void load_and_run_elf(char* exe) {
 
   // 2. Iterate through the PHDR table and find the section of PT_LOAD 
   //    type that contains the address of the entrypoint method in fib.c
-
   // for(int i = 0; i < ehdr -> e_phnum; i++){
   //   phdr = malloc(ehdr -> e_phnum * sizeof(Elf32_Phdr));
   //   read(fd,phdr,sizeof(Elf32_Phdr));
-  //   if (phdr -> p_type == 1 && phdr -> p_vaddr <= ehdr -> e_entry && phdr -> p_vaddr + 0x1000 > ehdr -> e_entry){   // check the 1000 one 
+  //   if (phdr -> p_type == 1 && phdr -> p_vaddr <= ehdr -> e_entry && phdr -> p_vaddr + 0x1000 > ehdr -> e_entry){
   //     segmntHdr = phdr;
   //   }
   // }
-
   // second program header check 
   // printf("%x\n", phdr->p_align);
 
+  struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = seg_handler;
+
+    if (sigaction(SIGSEGV, &sa, NULL) == -1) {
+        perror("Error setting up signal handler");
+        exit(1);
+    }
+
+
+
   // 3. Allocate memory of the size "p_memsz" using mmap function 
   //    and then copy the segment content
-  
+  void * virtual_mem = mmap(NULL, segmntHdr->p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
   // printf("%p\n", virtual_mem);
-  
+
   char * virtual_memC = virtual_mem;
   // printf("%s\n", virtual_memC);
  
@@ -139,47 +178,16 @@ void load_and_run_elf(char* exe) {
   printf("User _start return value = %d\n",result);
 }
 
-// Signal handler for segmentation faults
-void seg_handler(int signum, siginfo_t *info, void *context) {
-    printf("Segmentation fault at address %p\n", info->si_addr);
-
-    // Allocate memory using mmap
-    virtual_mem = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
-
-
-    // Load and copy content into allocated memory
-    // ...
-
-    // Update program counter or instruction pointer
-    // ...
-
-    // Update statistics
-    pageFault++;
-    pageAllocated++;
-    // internal_fragmentation += /* calculate internal fragmentation based on your logic */;
-
-    // Resume execution
-    return;
-}
-
 int main(int argc, char** argv) 
 {
   if(argc != 2) {
     printf("Usage: %s <ELF Executable> \n",argv[0]);
     exit(1);
   }
-  
-  struct sigaction sa;
-  sa.sa_flags = SA_SIGINFO;
-  sa.sa_sigaction = seg_handler;
-
-  if (sigaction(SIGSEGV, &sa, NULL) == -1) {
-      perror("Error setting up signal handler");
-      exit(1);
-  }
   // 1. carry out necessary checks on the input ELF file
   // 2. passing it to the loader for carrying out the loading/execution
   load_and_run_elf(argv[1]);
+  printf("hi");
   // 3. invoke the cleanup routine inside the loader  
   loader_cleanup();
   return 0;
